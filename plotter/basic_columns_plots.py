b9f478edb3f830be2df4be5dd1c69d57fa3f9a02
KonstiDE <konstantin.mueller1@gmx.de>
2023-03-12 23:06:38 +0100

    wrapper.py update and plot dates per day in quarter-hourly

--- plotter/basic_columns_plots.py
+++ plotter/basic_columns_plots.py
@@ -1,8 +1,10 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
 from .wrapper import wrap_plot as plot
+from .wrapper import wrap_plots as mplot
 
 
 def plot_source(df):
@@ -13,7 +15,18 @@ def plot_source(df):
 
 
 def plot_dates(df):
-    df["created_at"] = datetime.datetime.fromisoformat(df["created_at"])
+    df["created_at"] = pd.to_datetime(df["created_at"])
+    df["created_at"] = df["created_at"].dt.round('15min')
 
+    group2021_30 = df[df["created_at"].dt.day == 30].groupby(by="created_at").size().reset_index(name="count")
+    group2021_31 = df[df["created_at"].dt.day == 31].groupby(by="created_at").size().reset_index(name="count")
+    group2022_01 = df[df["created_at"].dt.day == 1].groupby(by="created_at").size().reset_index(name="count")
+    group2022_02 = df[df["created_at"].dt.day == 2].groupby(by="created_at").size().reset_index(name="count")
 
+    mplot(group2021_30["created_at"], y_datas=[
+        group2021_30["count"],
+        group2021_31["count"],
+        group2022_01["count"],
+        group2022_02["count"]
+    ], colors=["#ff7f00", "red", "green", "#007acc"], plot_type="line", grid="--")
 
--- plotter/wrapper.py
+++ plotter/wrapper.py
@@ -1,16 +1,61 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+import shutup
+shutup.please()
 
 
-def wrap_plot(x_data, y_data, plot_type="line", color="#007acc"):
+def wrap_plot(x_data, y_data, plot_type="line", color="#007acc", alpha=1, xticks=None, grid=None):
     fig, ax = plt.subplots()
     fig.patch.set_facecolor('#0c1c23')
 
     if plot_type == "line":
-        ax.plot(x_data, y_data, color=color, alpha=0.6)
+        ax.plot(x_data, y_data, color=color, alpha=alpha)
     elif plot_type == "bar":
-        ax.bar(x_data, y_data, color=color, alpha=0.6)
+        ax.bar(x_data, y_data, color=color, alpha=alpha)
     elif plot_type == "scatter":
-        ax.scatter(x_data, y_data, color=color, alpha=0.6)
+        ax.scatter(x_data, y_data, color=color, alpha=alpha)
+
+    ax.set_facecolor('#122229')
+    ax.spines['left'].set_color('#FFFFFF')
+    ax.spines['left'].set_linewidth(0.5)
+
+    ax.spines['bottom'].set_color("#3C494F")
+    ax.spines['bottom'].set_linewidth(0.3)
+    ax.spines['top'].set_color("#3C494F")
+    ax.spines['top'].set_linewidth(0.3)
+    ax.spines['right'].set_color("#3C494F")
+    ax.spines['right'].set_linewidth(0.3)
+
+    ax.tick_params(axis='both', colors='#FFFFFF')
+
+    if xticks is not None:
+        ax.set_xticklabels(xticks)
+
+    if grid is not None:
+        plt.grid(color="#3C494F", linestyle=grid, linewidth=0.3)
+
+    plt.xticks(rotation=45, ha='right', color="white")
+    plt.yticks(color="white")
+    plt.tight_layout()
+    plt.show()
+
+
+def wrap_plots(x_data, y_datas, colors, plot_type="line", alpha=1, xticks=None, grid=None):
+    assert len(colors) == len(y_datas)
+
+    fig, ax = plt.subplots()
+    fig.patch.set_facecolor('#0c1c23')
+
+    if plot_type == "line":
+        for idx, y_data in enumerate(y_datas):
+            ax.plot(x_data, y_data, color=colors[idx], alpha=alpha)
+    elif plot_type == "bar":
+        for idx, y_data in enumerate(y_datas):
+            ax.bar(x_data, y_data, color=colors[idx], alpha=alpha)
+    elif plot_type == "scatter":
+        for idx, y_data in enumerate(y_datas):
+            ax.scatter(x_data, y_data, color=colors[idx], alpha=alpha)
 
     ax.set_facecolor('#122229')
     ax.spines['left'].set_color('#FFFFFF')
@@ -27,5 +72,12 @@ def wrap_plot(x_data, y_data, plot_type="line", color="#007acc"):
 
     plt.xticks(rotation=45, ha='right', color="white")
     plt.yticks(color="white")
+
+    if xticks is not None:
+        ax.set_xticklabels(xticks)
+
+    if grid is not None:
+        plt.grid(color="#3C494F", linestyle=grid, linewidth=0.3)
+
     plt.tight_layout()
     plt.show()
